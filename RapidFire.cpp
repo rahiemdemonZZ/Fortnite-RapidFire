13e01c78c0612c291c4f8f88595972ef6d8d7c65
Real Reaper <95001569+ReallReaper@users.noreply.github.com>
2022-11-09 11:23:11 -0300

    Add files via upload

---
+++ RapidFire.cpp
@@ -0,0 +1,7 @@
+rapid fire (FireUwU)
+uintptr_t CurrentWeapon = vmread<uintptr_t>(settings::LocalPawn + 0x868); // FortPawn::CurrentWeapon 0x868
+Writee<float>(CurrentWeapon + 0x64, 70); // Actor::CustomTimeDilation 0x64
+big players (FireUwU)
+
+uintptr_t mesh = vmread<uintptr_t>(entity.Actor + 0x310);
+float BigPlayersScale = 50.f;
